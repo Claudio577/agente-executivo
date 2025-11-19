8fa7a4c2217d80e916d5fd23dd1982a0f2358c6c
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 15:05:56 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ load_dotenv()
 def get_llm():
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    def call_model(messages):
+    def call(messages):
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=messages,
-            temperature=0.1,
+            temperature=0.1
         )
         return response.choices[0].message["content"]
 
-    return call_model
+    return call
