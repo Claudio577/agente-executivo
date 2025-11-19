49a16145f67c667d188f6e8a319c73e6908aecff
Claudio577 <chideki.yoshida@gmail.com>
2025-11-19 14:29:58 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+def get_llm():
+    return init_chat_model(
+        model="gpt-4o-mini",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        temperature=0.1,
+    )
